49e04dd58f51f692b10486d9bd21a091aa9bbdf8
Eduard Samuel <samueledi80@hotmail.com>
2019-08-26 12:38:29 +0200

    Layout

---
+++ src/components/layout/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Aux from '../../hoc/Auxiliary';
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbat, SideDrawer, BackDrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+
+);
+
+export default layout;
