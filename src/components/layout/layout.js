e033232a6f6b481cf9fb8291b23787154030dfb8
Eduard Samuel <samueledi80@hotmail.com>
2019-08-26 14:34:53 +0200

    Ingredient.js added

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbat, SideDrawer, BackDrop</div>
-        <main>
+        <div >Toolbat, SideDrawer, BackDrop</div>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
