bbec5fc019ef0faecf5a5173393477fb605cf378
Eduard Samuel <samueledi80@hotmail.com>
2019-08-27 11:19:18 +0200

    BuildControls added to the screen

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,25 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
+    //Transforming the ingredients object of the state into an array.
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el) 
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Start adding ingredients!</p>
+    }
+
     return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="bread-bottom"/>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
         </div>
     );
 };
