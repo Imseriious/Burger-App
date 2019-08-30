697c622243d122d5ed4cdf24c4568318158637ad
Eduard Samuel <samueledi80@hotmail.com>
2019-08-30 19:09:33 +0200

    Perfomance and folder structure

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => ( //Second way of controlling height of logo with props
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
         <div className={classes.DesktopOnly} style={{height: '100%'}}>
             <Logo height={"80%"} />
         </div>
