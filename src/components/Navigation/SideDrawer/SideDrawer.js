2e668d7f480684951b23364f285e09a7526a6906
Eduard Samuel <samueledi80@hotmail.com>
2019-08-30 13:23:08 +0200

    Backdrop added to SideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,26 @@ import React from 'react';
 import classes from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Closed];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return ( //We wrap the logo so we can adjust the height just for this component (Other way on Toolbar.js)
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}> 
-                <Logo />
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
